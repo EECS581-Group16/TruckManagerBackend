664f4826e83761feb2d509f1f40976624b795fc2
masonxotto <masonxotto@gmail.com>
2022-11-20 17:58:03 -0600

    added login get and post requests and tested some values for inserting

--- server.js
+++ server.js
@@ -26,12 +26,33 @@ app.get("/invoices", (req, res) => {
 });
 
 //posts new data into database -- currently harcoded for testing, will eventually take user input from front end
+//req will be used to input query data once we have this connected to our front end
 app.post("/invoices", (req, res) => {
     const q = "INSERT INTO accounting.load_tickets_test (`ticket_number`,`customer`,`date`,`description`,`order`,`job`,`driver_id`,`truck_number`,`hours`,`tons`,`rate`,`driver_rate`) VALUES (?)";
     const values = [98288, "JMF", "2022-09-21T05:00:00.000Z", "Plantt 1 / 0175A23112", "23112", "175", "IL", "304", 69, 32.09, 69, 69];
     connection.query(q, [values], (err, result, fields) => {
         if (err) return res.json(err);
         return res.json("Invoice created successfully!");
+    });
+});
+
+app.get("/loginData", (req, res) => {
+    const q = "SELECT * FROM Login.Login;"; //retrieves every entry in login table
+    connection.query(q, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    });
+});
+
+//creates new row in Login table
+//req will be used to input query data once we have this connected to our front end
+//for testing purposes we are using hardcoded values, sprint 6 will connect this to the front end
+app.post("/login", (req, res) => {
+    const q = "INSERT INTO Login.Login(`Employee_ID`, `Username`, `Passcode`, `Email`) VALUES (?)";
+    const values = [1234, "admin", "password", "admin@admin.com"];
+    connection.query(q, [values], (err, result, fields) => {
+        if (err) return res.json(err);
+        return res.json("User created successfully!");
     });
 });
 
